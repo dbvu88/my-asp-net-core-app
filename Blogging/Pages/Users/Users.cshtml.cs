f66b805daad79752c82c031e386339b972ccd714
Duc Vu <de.yd315@gmail.com>
2019-11-02 08:45:44 -0500

    created user view

---
+++ Blogging/Pages/Users/Users.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Blogging.Pages.Users
+{
+    public class UsersModel : PageModel
+    {
+        private readonly IConfiguration config;
+
+        public String Message;
+
+        public UsersModel(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public void OnGet()
+        {
+            Message = config["Message"];
+        }
+    }
+}
