9ff98550151aaccf92ba8973dffb53514f810c72
Duc Vu <de.yd315@gmail.com>
2019-11-02 09:39:57 -0500

    connected data to the view

--- Blogging/Pages/Users/Users.cshtml.cs
+++ Blogging/Pages/Users/Users.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blogging.Core;
+using Blogging.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -11,16 +13,20 @@ namespace Blogging.Pages.Users
     public class UsersModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IUserData userData;
 
-        public String Message;
+        public string Message;
+        public IEnumerable<User> Users;
 
-        public UsersModel(IConfiguration config)
+        public UsersModel(IConfiguration config, IUserData userData)
         {
             this.config = config;
+            this.userData = userData;
         }
         public void OnGet()
         {
             Message = config["Message"];
+            Users = userData.GetAll();
         }
     }
 }
